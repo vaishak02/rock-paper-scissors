3ed7e7211049039cdb538e07b6d58b4c68ded8bc
vaishak02 <mvaishak02@gmail.com>
2023-10-23 14:27:23 +0530

    made functional buttons

--- game.js
+++ game.js
@@ -6,48 +6,51 @@ function getComputerChoice(choice) {
 
 function oneRound() {
   choice = ["rock", "paper", "scissors"];
-  let playerChoice = prompt("Choose Play: ").toLowerCase();
+  let playerChoice = this.textContent;
   let computerChoice = getComputerChoice(choice);
   if (playerChoice === computerChoice) {
-    return "It's a tie!";
-  }
-
-  if (
+    console.log("It's a tie!");
+  } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
-    return `You win! ${playerChoice} beats ${computerChoice}`;
-  }
-
-  return `You lose! ${computerChoice} beats ${playerChoice}`;
-}
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 1; i <= 5; i++) {
-    console.log(`Round ${i}:`);
-    const result = oneRound();
-    console.log(result);
-
-    if (result.includes("win")) {
-      playerScore++;
-    } else if (result.includes("lose")) {
-      computerScore++;
-    }
-  }
-
-  console.log("Game Over!");
-  console.log(`Player Score: ${playerScore}`);
-  console.log(`Computer Score: ${computerScore}`);
-
-  if (playerScore > computerScore) {
-    console.log("You win the game!");
-  } else if (computerScore > playerScore) {
-    console.log("Computer wins the game!");
+    console.log(`You win! ${playerChoice} beats ${computerChoice}`);
   } else {
-    console.log("It's a tie game!");
+    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
   }
 }
+
+// function game() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+
+//   for (let i = 1; i <= 5; i++) {
+//     console.log(`Round ${i}:`);
+//     const result = oneRound();
+//     console.log(result);
+
+//     if (result.includes("win")) {
+//       playerScore++;
+//     } else if (result.includes("lose")) {
+//       computerScore++;
+//     }
+//   }
+
+//   console.log("Game Over!");
+//   console.log(`Player Score: ${playerScore}`);
+//   console.log(`Computer Score: ${computerScore}`);
+
+//   if (playerScore > computerScore) {
+//     console.log("You win the game!");
+//   } else if (computerScore > playerScore) {
+//     console.log("Computer wins the game!");
+//   } else {
+//     console.log("It's a tie game!");
+//   }
+// }
+
+const btns = document.querySelectorAll("#btn");
+btns.forEach((btn) => {
+  btn.addEventListener("click", oneRound);
+});
