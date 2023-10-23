24b1a29ea0d2215a208d82ef8fa25fde5eb2a51b
vaishak02 <mvaishak02@gmail.com>
2023-10-23 17:49:42 +0530

    functionality added

--- game.js
+++ game.js
@@ -1,56 +1,75 @@
-function getComputerChoice(choice) {
-  let chosen_idx = Math.floor(Math.random() * 3);
-  const computerChoice = choice[chosen_idx];
-  return computerChoice;
+const optionBtn = document.querySelectorAll("div.optionBtn button");
+const roundResults = document.querySelector("#roundResults");
+const playerPoints = document.querySelector("#playerScore");
+const computerPoints = document.querySelector("#computerScore");
+const resetBtn = document.querySelector("#reset");
+
+//refresh page for new game
+resetBtn.addEventListener("click", () => location.reload());
+
+optionBtn.forEach((button) => {
+  button.addEventListener("click", getPlayerChoice);
+});
+
+let computerChoices = [
+  { choice: "Rock", value: 0 },
+  { choice: "Paper", value: 1 },
+  { choice: "Scissors", value: 2 },
+];
+let playerScore = 0;
+let compScore = 0;
+let playerChoice;
+
+function computerPlay() {
+  let result =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  return result;
 }
 
-function oneRound() {
-  choice = ["rock", "paper", "scissors"];
-  let playerChoice = this.textContent;
-  let computerChoice = getComputerChoice(choice);
-  if (playerChoice === computerChoice) {
-    console.log("It's a tie!");
-  } else if (
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "paper" && computerChoice === "rock") ||
-    (playerChoice === "scissors" && computerChoice === "paper")
-  ) {
-    console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+function playRound(playerSelection, computerSelection) {
+  let roundWinCombo = `${playerSelection}-${computerSelection.value}`;
+  let playerWinCombo = ["1-0", "0-2", "2-1"];
+
+  if (Number(playerSelection) === computerSelection.value) {
+    roundResults.textContent = "Tie!";
+  } else if (playerWinCombo.includes(roundWinCombo)) {
+    playerPoints.textContent = ++playerScore;
+    roundResults.textContent = `You win! ${playerChoice} beats ${computerSelection.choice}`;
   } else {
-    console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+    computerPoints.textContent = ++compScore;
+    roundResults.textContent = `You lose! ${computerSelection.choice} beats ${playerChoice}`;
+  }
+  checkWinner();
+}
+
+const winnerResults = {
+  computer: ["You Lost the game to a computer!", "red"],
+  player: ["You Win the game!!!!", "green"],
+  tie: ["The game is a Tie!", "blue"],
+};
+
+function checkWinner() {
+  if (compScore === 5 || playerScore === 5) {
+    if (compScore === playerScore) {
+      updateWinner("tie");
+    } else {
+      let win = `${compScore > playerScore ? "computer" : "player"}`;
+      updateWinner(win);
+    }
   }
 }
 
-// function game() {
-//   let playerScore = 0;
-//   let computerScore = 0;
+function updateWinner(winner) {
+  roundResults.textContent = winnerResults[winner][0];
+  roundResults.style.color = winnerResults[winner][1];
 
-//   for (let i = 1; i <= 5; i++) {
-//     console.log(`Round ${i}:`);
-//     const result = oneRound();
-//     console.log(result);
+  optionBtn.forEach((button) => {
+    button.removeEventListener("click", getPlayerChoice);
+  });
+}
 
-//     if (result.includes("win")) {
-//       playerScore++;
-//     } else if (result.includes("lose")) {
-//       computerScore++;
-//     }
-//   }
-
-//   console.log("Game Over!");
-//   console.log(`Player Score: ${playerScore}`);
-//   console.log(`Computer Score: ${computerScore}`);
-
-//   if (playerScore > computerScore) {
-//     console.log("You win the game!");
-//   } else if (computerScore > playerScore) {
-//     console.log("Computer wins the game!");
-//   } else {
-//     console.log("It's a tie game!");
-//   }
-// }
-
-const btns = document.querySelectorAll("#btn");
-btns.forEach((btn) => {
-  btn.addEventListener("click", oneRound);
-});
+function getPlayerChoice(e) {
+  let playerSelection = e.target.id;
+  playerChoice = e.target.textContent;
+  playRound(playerSelection, computerPlay());
+}
