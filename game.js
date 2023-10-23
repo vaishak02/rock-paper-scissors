7ed8c8753f27d5a5acdd72a1f86820c32d65747f
vaishak02 <mvaishak02@gmail.com>
2023-10-23 17:56:16 +0530

    update game.js

--- game.js
+++ game.js
@@ -73,3 +73,58 @@ function getPlayerChoice(e) {
   playerChoice = e.target.textContent;
   playRound(playerSelection, computerPlay());
 }
+
+//old code
+// function getComputerChoice(choice) {
+//   let chosen_idx = Math.floor(Math.random() * 3);
+//   const computerChoice = choice[chosen_idx];
+//   return computerChoice;
+// }
+
+// function oneRound() {
+//   choice = ["rock", "paper", "scissors"];
+//   let playerChoice = prompt("Choose Play: ").toLowerCase();
+//   let computerChoice = getComputerChoice(choice);
+//   if (playerChoice === computerChoice) {
+//     return "It's a tie!";
+//   }
+
+//   if (
+//     (playerChoice === "rock" && computerChoice === "scissors") ||
+//     (playerChoice === "paper" && computerChoice === "rock") ||
+//     (playerChoice === "scissors" && computerChoice === "paper")
+//   ) {
+//     return `You win! ${playerChoice} beats ${computerChoice}`;
+//   }
+
+//   return `You lose! ${computerChoice} beats ${playerChoice}`;
+// }
+
+// function game() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+
+//   for (let i = 1; i <= 5; i++) {
+//     console.log(`Round ${i}:`);
+//     const result = oneRound();
+//     console.log(result);
+
+//     if (result.includes("win")) {
+//       playerScore++;
+//     } else if (result.includes("lose")) {
+//       computerScore++;
+//     }
+//   }
+
+//   console.log("Game Over!");
+//   console.log(`Player Score: ${playerScore}`);
+//   console.log(`Computer Score: ${computerScore}`);
+
+//   if (playerScore > computerScore) {
+//     console.log("You win the game!");
+//   } else if (computerScore > playerScore) {
+//     console.log("Computer wins the game!");
+//   } else {
+//     console.log("It's a tie game!");
+//   }
+// }
